325fe29f431811d459733b358713fc89f8668d4b
Vishal Singh <115693661+vishal-2023@users.noreply.github.com>
2023-07-25 11:37:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./component/Header";
+import Footer from "./component/Footer";
+import Notes from "./component/Notes";
+import Input from "./component/Input"
+function App(){
+    return(
+        <div>
+        <Header />
+        <Input />
+        <Footer />
+        </div>
+        
+    )
+}
+
+export default App;
